3bb4d4970e6632942ee49a7c03ab22268e513135
flashsoon <developsoon@gmail.com>
2023-07-25 15:54:10 +0900

    Algorithm/Programmers/javascript/programmers_lv2_숫자변환하기 solved

---
+++ Algorithm/Programmers/javascript/programmers_lv2_숫자변환하기/s1.js
@@ -0,0 +1,25 @@
+function solution(x, y, n) {
+  const dp = Array.from(new Array(y + 1), () => 1000001);
+  dp[x] = 0;
+  for (let i = x; i < dp.length; i++) {
+    if (i !== x && dp[i] === 1000001) {
+      continue;
+    }
+
+    if (i + n < dp.length) {
+      dp[i + n] = Math.min(dp[i] + 1, dp[i + n]);
+    }
+    if (i * 2 < dp.length) {
+      dp[2 * i] = Math.min(dp[i] + 1, dp[2 * i]);
+    }
+    if (i * 3 < dp.length) {
+      dp[3 * i] = Math.min(dp[i] + 1, dp[3 * i]);
+    }
+  }
+
+  if (dp[y] === 1000001) {
+    return -1;
+  }
+
+  return dp[y];
+}
